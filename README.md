a2594f372d33f2c07dce84bbfe1edef5aa479632
flippette <quangdatle2006@outlook.com>
2024-01-24 06:21:55 +0700

    move to stow for symlink management

---
+++ README.md
@@ -0,0 +1,9 @@
+# dotfiles
+
+My config files.
+
+## Installing
+
+```sh
+  stow -R . -t $XDG_CONFIG_HOME
+```
--- setup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
-
-ln -s -t "$XDG_CONFIG_HOME" $(find . -maxdepth 1 -type d ! -name '.*' | cut -c3-)
