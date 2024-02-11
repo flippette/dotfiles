5383fc5c3b2c20ce7094fa97f8ac992f074bb867
flippette <quangdatle2006@outlook.com>
2024-02-12 03:51:42 +0700

    stuff

--- fish/fish/config.fish
+++ fish/fish/config.fish
@@ -1,21 +1,15 @@
 set -g fish_greeting
 
 if status is-login
-    set -gx PATH "$HOME/documents/scripts:$HOME/.local/bin:$HOME/.cargo/bin:$PATH"
-    sudo amdgpu-oc
+    set -gx PATH "$HOME/.local/bin:$HOME/.cargo/bin:$PATH"
 end
-
-set -gx EDITOR hx
-set -gx VISUAL hx
 
 if status is-interactive
     pfetch
     starship init fish | source
 
-    # aliases
     alias ls  "exa --icons=auto"
     alias ll  "ls -l"
     alias cat "bat"
     alias du  "dust"
-    alias rm  "echo use trash instead!"
 end
