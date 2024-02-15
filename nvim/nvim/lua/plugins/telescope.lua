0464686b0fae438272b88124dfceb51881df7427
flippette <quangdatle2006@outlook.com>
2024-02-15 19:44:15 +0700

    waybar stuff, change default telescope.nvim theme to dropdown

--- nvim/nvim/lua/plugins/telescope.lua
+++ nvim/nvim/lua/plugins/telescope.lua
@@ -1,9 +1,21 @@
 return {
   "nvim-telescope/telescope.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope-fzy-native.nvim",
+  },
   config = function()
-    local builtin = require("telescope.builtin")
+    local telescope = require("telescope")
+    telescope.setup({
+      defaults = require("telescope.themes").get_dropdown({
+        prompt_prefix = "",
+        selection_caret = "",
+        entry_prefix = "",
+      }),
+    })
+    telescope.load_extension("fzy_native")
 
+    local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<leader>f", builtin.find_files, { desc = "Find files" })
     vim.keymap.set("n", "<leader>/", builtin.live_grep, { desc = "Live grep" })
     vim.keymap.set("n", "<leader>?", builtin.keymaps, { desc = "Search keymaps" })
