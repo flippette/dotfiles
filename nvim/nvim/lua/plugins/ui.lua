17b06971e80033772e787e0e18fb2c5878c4876b
flippette <quangdatle2006@outlook.com>
2024-02-26 04:19:28 +0700

    replace nvim-notify with noice.nvim

---
+++ nvim/nvim/lua/plugins/ui.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "folke/noice.nvim",
+    dependencies = {
+      { "MunifTanjim/nui.nvim" },
+    },
+    opts = {
+      cmdline = { enabled = false },
+      messages = { enabled = false },
+      popupmenu = { enabled = false },
+      hover = { enabled = false },
+      signature = { enabled = false },
+    },
+  }
+}
