fe9dce792df621808829d5528ca86fd162f15ad7
flippette <quangdatle2006@outlook.com>
2024-03-06 13:56:13 +0700

    configure conform.nvim

--- nvim/nvim/lua/plugins/format.lua
+++ nvim/nvim/lua/plugins/format.lua
@@ -1,4 +1,20 @@
 return {
   "stevearc/conform.nvim",
-  opts = {},
+  opts = {
+    formatters_by_ft = {
+      html = { "rustywind" },
+      htmldjango = { "rustywind" },
+      javascript = { { "prettier", "rustywind" } },
+      javascriptreact = { { "prettier", "rustywind" } },
+      json = { "prettier" },
+      jsonc = { "prettier" },
+      lua = { "stylua" },
+      rust = { "rustywind" },
+      typescript = { { "prettier", "rustywind" } },
+      typescriptreact = { { "prettier", "rustywind" } },
+    },
+    format_after_save = {
+      lsp_fallback = true,
+    },
+  },
 }
