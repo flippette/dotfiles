af3273c1d9306d14b8716b067e712399b5c2e2e8
flippette <quangdatle2006@outlook.com>
2024-04-10 08:15:23 +0700

    make virtual text diagnostic backgrounds transparent

---
+++ nvim/nvim/lua/core/after.lua
@@ -0,0 +1,13 @@
+local function update_hl(ns_id, name, val)
+  local hl_opts = vim.api.nvim_get_hl(0, { name = name })
+  for k, v in pairs(val) do
+    hl_opts[k] = v
+  end
+  vim.api.nvim_set_hl(ns_id, name, hl_opts)
+end
+
+update_hl(0, "DiagnosticVirtualTextError", { bg = "NONE" })
+update_hl(0, "DiagnosticVirtualTextWarn", { bg = "NONE" })
+update_hl(0, "DiagnosticVirtualTextInfo", { bg = "NONE" })
+update_hl(0, "DiagnosticVirtualTextHint", { bg = "NONE" })
+update_hl(0, "DiagnosticVirtualTextOk", { bg = "NONE" })
