e2968d70faa4ce7d63ce560655c2f013cdabaf24
flippette <me@flippo.cc>
2025-07-27 23:02:10 +0700

    nix is required now

--- fish/config.fish
+++ fish/config.fish
@@ -1,14 +1,14 @@
 set -g fish_greeting
 
+source "/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish"
+fish_add_path $HOME/.nix-profile
+
 fish_add_path /opt/rocm/bin
 fish_add_path /opt/cuda/bin
 fish_add_path $HOME/.cargo/bin
 fish_add_path $HOME/.local/bin
 fish_add_path $HOME/.local/share/coursier/bin
 
-# nix is optional on my systems for now :shrug:
-set nix "/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish"
-test -e "$nix" && source "$nix"
 
 if status is-interactive
     set ZELLIJ_AUTO_EXIT true
